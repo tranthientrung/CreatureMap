1553137574c5eeb78245d82f94b53a00de175207
Trung Tran <trungtran190997@gmail.com>
2019-09-14 10:30:26 +0700

    Added babel

---
+++ Sources/Screens/Start/Start.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+class Start extends React.Component {
+  render() {
+    return <View style={{backgroundColor: 'blue', flex: 1}}></View>;
+  }
+}
+
+export default Start;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './Sources/App/App';
+import {name as appName} from './Sources/App/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
